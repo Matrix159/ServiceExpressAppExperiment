3dcc82e5621e3e69a005c29560684dc787512a5d
Josh <joseldridge15@gmail.com>
2018-09-08 16:14:57 -0400

    Working example of barcode scanning

--- app/src/main/java/com/serviceexpress/serviceexpressapp/MainActivity.kt
+++ app/src/main/java/com/serviceexpress/serviceexpressapp/MainActivity.kt
@@ -1,7 +1,16 @@
 package com.serviceexpress.serviceexpressapp
 
+import android.graphics.Bitmap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +21,45 @@ class MainActivity : AppCompatActivity() {
         //SEService.INSTANCE.listRepos("TestString")
 
         // Show an example using RxJava to show how it's easier with callbacks that way
+    }
+
+    fun shootCamera(v: View) {
+        camera.captureImage { cameraKitImage ->
+            // Get the Bitmap from the captured shot
+            getQRCodeDetails(cameraKitImage.bitmap)
+        }
+    }
+
+    private fun getQRCodeDetails(bitmap: Bitmap) {
+        val options = FirebaseVisionBarcodeDetectorOptions.Builder()
+                        .setBarcodeFormats(FirebaseVisionBarcode.FORMAT_ALL_FORMATS)
+                        .build()
+        val detector = FirebaseVision.getInstance().getVisionBarcodeDetector(options)
+        val image = FirebaseVisionImage.fromBitmap(bitmap)
+        detector.detectInImage(image)
+                .addOnSuccessListener {
+                    if (it.isEmpty()) {
+                        content.text = "No barcode found"
+                        return@addOnSuccessListener
+                    }
+                    for (firebaseBarcode in it) {
+                        content.text = firebaseBarcode.displayValue //Display contents inside the barcode
+                        //sheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+                    }
+                }
+                .addOnFailureListener {
+                    it.printStackTrace()
+                    Toast.makeText(baseContext, "Sorry, something went wrong!", Toast.LENGTH_SHORT).show()
+                }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        camera.start()
+    }
+
+    override fun onPause() {
+        camera.stop()
+        super.onPause()
     }
 }
